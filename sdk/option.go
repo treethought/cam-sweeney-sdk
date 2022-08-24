fd7a163072371599e7ac3339af63fb36dc7fd40c
Cam Sweeney <cpsweene@gmail.com>
2022-08-24 16:33:41 -0700

    add initial request options

---
+++ sdk/option.go
@@ -0,0 +1,53 @@
+package sdk
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type RequestOption func(*http.Request)
+
+type PaginationOptions struct {
+	Limit  int
+	Page   int
+	Offset int
+}
+
+func withQuery(key string, val string) RequestOption {
+	return func(req *http.Request) {
+		req.URL.Query().Add(key, val)
+	}
+}
+
+func WithLimit(limit int) RequestOption {
+	return withQuery("limit", fmt.Sprint(limit))
+}
+
+func WithPage(p int) RequestOption {
+	return withQuery("page", fmt.Sprint(p))
+}
+
+func WithOffset(offset int) RequestOption {
+	return withQuery("page", fmt.Sprint(offset))
+}
+
+func WithPagination(opts PaginationOptions) RequestOption {
+	return func(req *http.Request) {
+		if opts.Offset > 0 {
+			req.URL.Query().Add("offset", fmt.Sprint(opts.Offset))
+		}
+		if opts.Limit > 0 {
+			req.URL.Query().Add("limit", fmt.Sprint(opts.Limit))
+		}
+		if opts.Page > 0 {
+			req.URL.Query().Add("page", fmt.Sprint(opts.Page))
+		}
+	}
+}
+
+func WithSort(field string, dir string) RequestOption {
+	return func(req *http.Request) {
+		val := fmt.Sprintf("%s:%s", field, dir)
+		req.URL.Query().Add("sort", fmt.Sprint(val))
+	}
+}
